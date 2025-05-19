0b8a4d5ac264e5d310140eb3c84871d0625295c7
Puthyrath Hy <puthyrathy@gmail.com>
2025-05-18 20:03:05 -0700

    fix playwright ports

--- playwright.config.js
+++ playwright.config.js
@@ -75,7 +75,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run start',
-    url: 'http://localhost:5001',
+    url: 'http://localhost:8080',
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
   }
