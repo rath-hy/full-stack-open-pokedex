b5efbf5aa3e599488272015dc9069e7245db067e
Puthyrath Hy <puthyrathy@gmail.com>
2025-05-19 15:31:06 -0700

    Do 11.12

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const PORT = 5001
 
 app.use(express.static('dist'))
 
+app.get('/health', (req, res) => {
+
+  // if (true) throw('error...  ')
+  res.send('ok')
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
